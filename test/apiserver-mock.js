0be980c6601c1be7f673c817c7d6f9681c8c76de
Evan Prodromou <evan@prodromou.name>
2018-07-31 13:10:39 -0400

    Decaffeinate source code

---
+++ test/apiserver-mock.js
@@ -0,0 +1,373 @@
+// apiserver-mock.coffee -- mock interface for testing the fuzzy.ai class
+//
+// Copyright 2014-2016 Fuzzy.ai <node@fuzzy.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const http = require('http');
+const urlparse = require('url').parse;
+
+const _ = require('lodash');
+
+const JSON_TYPE = "application/json; charset=utf-8";
+const EVALUATION_ID = "ersatzersatz";
+
+class APIServerMock {
+  constructor(userID, token) {
+    const ids = [];
+    const agent1 = {
+      id: "fakefakefakefake",
+      userID,
+      latestVersion: "ekafekafekafekaf",
+      name: "temperature",
+      inputs: {
+        temperature: {
+          cold: [50, 75],
+          normal: [50, 75, 85, 100],
+          hot: [85, 100]
+        }
+      },
+      outputs: {
+        fanSpeed: {
+          slow: [50, 100],
+          normal: [50, 100, 150, 200],
+          fast: [150, 200]
+        }
+      },
+      rules: [
+        "IF temperature IS cold THEN fanSpeed IS slow",
+        "IF temperature IS normal THEN fanSpeed IS normal",
+        "IF temperature IS hot THEN fanSpeed IS fast"
+      ],
+      createdAt: "2014-12-17T18:18:14.850Z",
+      updatedAt: "2014-12-17T18:18:14.850Z"
+    };
+    const version1 = {
+      id: "ekafekafekafekaf",
+      userID,
+      versionOf: "fakefakefakefake",
+      inputs: {
+        temperature: {
+          cold: [50, 75],
+          normal: [50, 75, 85, 100],
+          hot: [85, 100]
+        }
+      },
+      outputs: {
+        fanSpeed: {
+          slow: [50, 100],
+          normal: [50, 100, 150, 200],
+          fast: [150, 200]
+        }
+      },
+      rules: [
+        "IF temperature IS cold THEN fanSpeed IS slow",
+        "IF temperature IS normal THEN fanSpeed IS normal",
+        "IF temperature IS hot THEN fanSpeed IS fast"
+      ],
+      parsed_rules: [
+        {
+          type: "if-then",
+          antecedent: {
+            type: "is",
+            dimension: "temperature",
+            set: "cold"
+          },
+          consequent: {
+            type: "is",
+            dimension: "fanSpeed",
+            set: "slow"
+          }
+        },
+        {
+          type: "if-then",
+          antecedent: {
+            type: "is",
+            dimension: "temperature",
+            set: "normal"
+          },
+          consequent: {
+            type: "is",
+            dimension: "fanSpeed",
+            set: "normal"
+          }
+        },
+        {
+          type: "if-then",
+          antecedent: {
+            type: "is",
+            dimension: "temperature",
+            set: "hot"
+          },
+          consequent: {
+            type: "is",
+            dimension: "fanSpeed",
+            set: "fast"
+          }
+        }
+      ],
+
+      createdAt: "2014-12-17T18:18:14.850Z"
+    };
+    const agents = [
+      agent1
+    ];
+    const evaluation1 = {
+      reqID: EVALUATION_ID,
+      userID,
+      agentID: "fakefakefakefake",
+      versionID: "ekafekafekafekaf",
+      input: {
+        temperature: 75
+      },
+      rules: [
+        1,
+        2
+      ],
+      inferred: {
+        fanSpeed: {
+          slow: 0.3333,
+          normal: 0.5555,
+          fast: 0.75
+        }
+      },
+      clipped: {
+        fanSpeed: {
+          slow: [[0, 0.0], [1, 0.85], [3, 0.85], [4, 0]],
+          normal: [[2, 0.0], [3, 0.85], [5, 0.85], [6, 0]],
+          fast: [[4, 0.0], [7, 0.85], [9, 0.85], [10, 0]]
+        }
+      },
+      combined: {
+        fanSpeed: [
+          [0, 7],
+          [0, 9],
+          [1, 1]
+        ]
+      },
+      centroid: {
+        fanSpeed: [3.3, 17]
+      },
+      crisp: {
+        fanSpeed: 17
+      }
+    };
+
+    const resp = function(response, code, body, headers) {
+      if (headers == null) { headers = {}; }
+      response.statusCode = code;
+      response.setHeader("Content-Type", JSON_TYPE);
+      for (let value = 0; value < headers.length; value++) {
+        const name = headers[value];
+        response.setHeader(name, value);
+      }
+      return response.end(JSON.stringify(body));
+    };
+
+    const routes = [
+      ["GET", "^/agent$", (request, response, match) => resp(response, 200, agents)
+      ],
+      ["POST", "^/agent$", function(request, response, match) {
+        if (!_.isObject(request.body)) {
+          resp(response, 400, {status: "error", message: "Body not an object"});
+          return;
+        }
+        if (!_.has(request.body, "inputs")) {
+          resp(response, 400, {status: "error", message: "Body has no inputs"});
+          return;
+        }
+        if (!_.has(request.body, "outputs")) {
+          resp(response, 400, {status: "error", message: "Body has no outputs"});
+          return;
+        }
+        if (!_.has(request.body, "rules")) {
+          resp(response, 400, {status: "error", message: "Body has no rules"});
+          return;
+        }
+        return resp(response, 200, agent1);
+      }
+      ],
+      ["GET", "^/agent/(.*?)$", (request, response, match) => resp(response, 200, agent1)
+      ],
+      ["POST", "^/agent/(.*?)$", function(request, response, match) {
+        if (!_.isObject(request.body)) {
+          resp(response, 400, {status: "error", message: "Body not an object"});
+          return;
+        }
+        if (_.keys(request.body).length === 0) {
+          resp(response, 400, {status: "error", message: "Body has no inputs"});
+          return;
+        }
+        const parsed = urlparse(request.url, true);
+        response.statusCode = 200;
+        response.setHeader("Content-Type", JSON_TYPE);
+        response.setHeader("X-Evaluation-ID", EVALUATION_ID);
+        const results = {fanSpeed: 45.3};
+        if (parsed.query.meta != null) {
+          let prop;
+          if (["true", "1", "yes"].includes(parsed.query.meta)) {
+            prop = 'meta';
+          } else {
+            prop = parsed.query.meta;
+          }
+          const toOmit = ["userID", "agentID", "versionID", "input", "crisp"];
+          results[prop] = _.omit(evaluation1, toOmit);
+        }
+        return response.end(JSON.stringify(results));
+      }
+      ],
+      ["GET", "^/evaluation/(.*?)$", (request, response, match) => resp(response, 200, evaluation1)
+      ],
+      ["POST", "^/evaluation/(.*?)/feedback$", function(request, response, match) {
+        if (!_.isObject(request.body)) {
+          resp(response, 400, {status: "error", message: "Body not an object"});
+          return;
+        }
+        if (_.keys(request.body).length === 0) {
+          resp(response, 400, {status: "error", message: "Body has no feedback"});
+          return;
+        }
+        response.statusCode = 200;
+        response.setHeader("Content-Type", JSON_TYPE);
+        return response.end(JSON.stringify(request.body));
+      }
+      ],
+      ["PUT", "^/agent/(.*?)$", function(request, response, match) {
+        if (!_.isObject(request.body)) {
+          resp(response, 400, {status: "error", message: "Body not an object"});
+          return;
+        }
+        if (!_.has(request.body, "inputs")) {
+          resp(response, 400, {status: "error", message: "Body has no inputs"});
+          return;
+        }
+        if (!_.has(request.body, "outputs")) {
+          resp(response, 400, {status: "error", message: "Body has no outputs"});
+          return;
+        }
+        if (!_.has(request.body, "rules")) {
+          resp(response, 400, {status: "error", message: "Body has no rules"});
+          return;
+        }
+        return resp(response, 200, agent1);
+      }
+      ],
+      ["DELETE", "^/agent/(.*?)$", (request, response, match) => resp(response, 200, {message: "OK"})
+      ],
+      ["GET", "^/version/(.*?)$", (request, response, match) => resp(response, 200, version1)
+      ],
+      ["GET", "^/version$", (request, response, match) =>
+        resp(response, 200, {
+          name: "apiserver-mock",
+          version: "0.24.0",
+          controllerVersion: "0.13.0"
+        }
+        )
+      
+      ]
+    ];
+
+    const server = http.createServer(function(request, response) {
+      let body = "";
+      const respond = function(code, body) {
+        response.statusCode = code;
+        if (!response.headersSent) {
+          response.setHeader("Content-Type", JSON_TYPE);
+        }
+        return response.end(JSON.stringify(body));
+      };
+      request.on("data", chunk => body += chunk);
+      request.on("error", err => respond(500, {status: "error", message: err.message}));
+      return request.on("end", function() {
+        const auth = request.headers.authorization;
+        // No need for auth for /version
+        if (request.url !== "/version") {
+          if (!auth) {
+            respond(403, {status: "error", message: "No Authorization header"});
+            return;
+          }
+          const am = auth.match(/^Bearer (.*?)$/);
+          if (!am) {
+            respond(403, {
+              status: "error",
+              message: "No token in Authorization header"
+            }
+            );
+            return;
+          }
+          if (am[1] !== token) {
+            respond(403, {
+              status: "error",
+              message: `Bad authorization token (${am[1]} != ${token})`
+            }
+            );
+            return;
+          }
+        }
+        if (["PUT", "POST"].includes(request.method)) {
+          if (body.length === 0) {
+            respond(400, {status: "error", message: "No content in request"});
+            return;
+          }
+
+          const type = request.headers['content-type'];
+          if (type.substr(0, "application/json".length) !== "application/json") {
+            respond(400, {
+              status: "error",
+              message: `Not a JSON request; Content-Type = ${type}`
+            }
+            );
+            return;
+          }
+
+          try {
+            request.body = JSON.parse(body);
+          } catch (err) {
+            respond(400, {status: "error", message: err.message});
+            return;
+          }
+        }
+
+        // Find a route
+        for (let route of Array.from(routes)) {
+          if (request.method === route[0]) {
+            const match = request.url.match(route[1]);
+            if (match) {
+              return route[2](request, response, match);
+            }
+          }
+        }
+        // If we get here, no route found
+        return respond(404, {
+          status: "error",
+          message: `Cannot ${request.method} ${request.url}`
+        }
+        );
+      });
+    });
+
+    this.start = function(callback) {
+      server.once('error', err => callback(err));
+      server.once('listening', () => callback(null));
+      return server.listen(2342);
+    };
+
+    this.stop = function(callback) {
+      server.once('close', () => callback(null));
+      server.once('error', err => callback(err));
+      return server.close();
+    };
+  }
+}
+
+module.exports = APIServerMock;
---
+++ test/module-test.js
@@ -0,0 +1,368 @@
+// module-test.coffee -- test core functionality of fuzzy.ai module
+//
+// Copyright 2014-2016 Fuzzy.ai <node@fuzzy.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+process.on('uncaughtException', err => console.error(err));
+
+const vows = require('vows');
+const assert = require('assert');
+
+const USERID = 'ersatz1234';
+const TOKEN = 'madeup5678';
+const AGENTID = 'spurious9101';
+const AGENTID2 = 'notreal1121';
+const VERSIONID = 'ekafekafekafekaf';
+
+const AGENT = {
+  inputs: {
+    input1: {
+      veryLow: [0, 20],
+      low: [5, 25, 45],
+      medium: [30, 50, 70],
+      high: [55, 75, 95],
+      veryHigh: [80, 100]
+    }
+  },
+  outputs: {
+    output1: {
+      veryLow: [0, 20],
+      low: [5, 25, 45],
+      medium: [30, 50, 70],
+      high: [55, 75, 95],
+      veryHigh: [80, 100]
+    }
+  },
+  rules: [
+    "IF input1 IS veryLow THEN output1 IS veryLow",
+    "IF input1 IS low THEN output1 IS low",
+    "IF input1 IS medium THEN output1 IS medium",
+    "IF input1 IS high THEN output1 IS high",
+    "IF input1 IS veryHigh THEN output1 IS veryHigh"
+  ]
+};
+
+vows
+  .describe('Basic fuzzy.ai test')
+  .addBatch({
+    'When we start the mock server': {
+      topic() {
+        const APIServerMock = require('./apiserver-mock');
+        const mock = new APIServerMock(USERID, TOKEN);
+        mock.start(err => {
+          return this.callback(err, mock);
+        });
+        return undefined;
+      },
+      'it works'(err, mock) {
+        assert.ifError(err);
+        return assert.isObject(mock);
+      },
+      'teardown'(mock) {
+        const { callback } = this;
+        mock.stop(err => callback(null));
+        return undefined;
+      },
+      'and we load the fuzzy.ai library': {
+        topic() {
+          try {
+            const mod = require('../lib/fuzzy.ai');
+            this.callback(null, mod);
+          } catch (err) {
+            this.callback(err, null);
+          }
+          return undefined;
+        },
+        'it works'(err, mod) {
+          assert.ifError(err);
+          return assert.isFunction(mod);
+        },
+        'and we start the module': {
+          topic(FuzzyAIClient) {
+            const { callback } = this;
+            try {
+              FuzzyAIClient.start();
+              callback(null, FuzzyAIClient);
+            } catch (err) {
+              callback(err);
+            }
+            return undefined;
+          },
+          'it works'(err, FuzzyAIClient) {
+            return assert.ifError(err);
+          },
+          'teardown'(FuzzyAIClient) {
+            return FuzzyAIClient.stop();
+          },
+          'and we create a client with the mock address': {
+            topic(FuzzyAIClient) {
+              try {
+                const client = new FuzzyAIClient(TOKEN, "http://localhost:2342");
+                this.callback(null, client);
+              } catch (err) {
+                this.callback(err, null);
+              }
+              return undefined;
+            },
+            'it works'(err, client) {
+              assert.ifError(err);
+              return assert.isObject(client);
+            },
+            teardown(client) {
+              const { callback } = this;
+              client.stop(err => callback(null));
+              return undefined;
+            },
+            'and we examine the client': {
+              topic(client) {
+                return client;
+              },
+              'it has the right public methods'(client) {
+                assert.isFunction(client.getAgents, "getAgents");
+                assert.isFunction(client.newAgent, "newAgent");
+                assert.isFunction(client.getAgent, "getAgent");
+                assert.isFunction(client.evaluate, "evaluate");
+                assert.isFunction(client.putAgent, "putAgent");
+                assert.isFunction(client.deleteAgent, "deleteAgent");
+                assert.isFunction(client.apiVersion, "apiVersion");
+                return assert.isFunction(client.getAgentVersion, "getAgentVersion");
+              },
+              'it has the inherited EventEmitter methods'(client) {
+                // There are others; these are commonly-used ones
+                assert.isFunction(client.on, "on");
+                assert.isFunction(client.once, "once");
+                return assert.isFunction(client.removeListener, "removeListener");
+              },
+              'and we check the API version': {
+                topic(client) {
+                  client.apiVersion(this.callback);
+                  return undefined;
+                },
+                'it works'(err, versionData) {
+                  return assert.ifError(err);
+                },
+                'it has the API version number'(err, versionData) {
+                  assert.ifError(err);
+                  assert.isObject(versionData);
+                  return assert.isString(versionData.version);
+                },
+                'it has the implementation name'(err, versionData) {
+                  assert.ifError(err);
+                  assert.isObject(versionData);
+                  return assert.isString(versionData.name);
+                },
+                'it has the controller version number'(err, versionData) {
+                  assert.ifError(err);
+                  assert.isObject(versionData);
+                  return assert.isString(versionData.controllerVersion);
+                }
+              },
+              'and we get the user agents': {
+                topic(client) {
+                  client.getAgents(this.callback);
+                  return undefined;
+                },
+                'it works'(err, agents) {
+                  assert.ifError(err);
+                  assert.isArray(agents);
+                  return (() => {
+                    const result = [];
+                    for (let agent of Array.from(agents)) {
+                      assert.isObject(agent);
+                      assert.isString(agent.id);
+                      result.push(assert.isString(agent.name));
+                    }
+                    return result;
+                  })();
+                }
+              },
+              'and we add a user agent': {
+                topic(client) {
+                  client.newAgent(AGENT, this.callback);
+                  return undefined;
+                },
+                'it works'(err, agent) {
+                  assert.ifError(err);
+                  return assert.isObject(agent);
+                }
+              },
+              'and we get an agent': {
+                topic(client) {
+                  client.getAgent(AGENTID, this.callback);
+                  return undefined;
+                },
+                'it works'(err, agent) {
+                  assert.ifError(err);
+                  return assert.isObject(agent);
+                }
+              },
+              'and we update an agent': {
+                topic(client) {
+                  client.putAgent(AGENTID, AGENT, this.callback);
+                  return undefined;
+                },
+                'it works'(err, agent) {
+                  assert.ifError(err);
+                  return assert.isObject(agent);
+                }
+              },
+              'and we delete an agent': {
+                topic(client) {
+                  client.deleteAgent(AGENTID2, this.callback);
+                  return undefined;
+                },
+                'it works'(err) {
+                  return assert.ifError(err);
+                }
+              },
+              'and we evaluate': {
+                topic(client) {
+                  client.evaluate(AGENTID, {input1: 69}, this.callback);
+                  return undefined;
+                },
+                'it works'(err, outputs) {
+                  assert.ifError(err);
+                  return assert.isObject(outputs);
+                }
+              },
+              'and we evaluate with the meta flag': {
+                topic(client) {
+                  client.evaluate(AGENTID, {input1: 69}, true, this.callback);
+                  return undefined;
+                },
+                'it works'(err, outputs) {
+                  assert.ifError(err);
+                  assert.isObject(outputs);
+                  assert.isObject(outputs.meta);
+                  return assert.isString(outputs.meta.reqID);
+                },
+                'and we get the audit information': {
+                  topic(outputs, client) {
+                    client.evaluation(outputs.meta.reqID, this.callback);
+                    return undefined;
+                  },
+                  'it works'(err, evaluation) {
+                    assert.ifError(err);
+                    return assert.isObject(evaluation);
+                  }
+                },
+                'and we provide feedback': {
+                  topic(outputs, client) {
+                    client.feedback(outputs.meta.reqID, {size: 13}, this.callback);
+                    return undefined;
+                  },
+                  'it works'(err, feedback) {
+                    assert.ifError(err);
+                    return assert.isObject(feedback);
+                  }
+                }
+              },
+              'and we evaluate with the meta flag set to a string value': {
+                topic(client) {
+                  client.evaluate(AGENTID, {input1: 69}, 'audit', this.callback);
+                  return undefined;
+                },
+                'it works'(err, outputs) {
+                  assert.ifError(err);
+                  assert.isObject(outputs);
+                  assert.isObject(outputs.audit);
+                  return assert.isString(outputs.audit.reqID);
+                },
+                'and we get the audit information': {
+                  topic(outputs, client) {
+                    client.evaluation(outputs.audit.reqID, this.callback);
+                    return undefined;
+                  },
+                  'it works'(err, evaluation) {
+                    assert.ifError(err);
+                    return assert.isObject(evaluation);
+                  }
+                },
+                'and we provide feedback': {
+                  topic(outputs, client) {
+                    client.feedback(outputs.audit.reqID, {size: 13}, this.callback);
+                    return undefined;
+                  },
+                  'it works'(err, feedback) {
+                    assert.ifError(err);
+                    return assert.isObject(feedback);
+                  }
+                }
+              },
+              'and we get an agent version': {
+                topic(client) {
+                  client.getAgentVersion(VERSIONID, this.callback);
+                  return undefined;
+                },
+                'it works'(err, version) {
+                  return assert.ifError(err);
+                },
+                'it looks correct'(err, data) {
+                  assert.ifError(err);
+                  assert.isObject(data);
+                  assert.isObject(data.inputs);
+                  assert.isObject(data.outputs);
+                  for (let rule of Array.from(data.rules)) {
+                    assert.isString(rule);
+                  }
+                  assert.isArray(data.parsed_rules);
+                  for (let parsed_rule of Array.from(data.parsed_rules)) {
+                    assert.isObject(parsed_rule);
+                  }
+                  assert.isString(data.id);
+                  assert.isString(data.userID);
+                  assert.isString(data.versionOf);
+                  return assert.isString(data.createdAt);
+                }
+              }
+            }
+          },
+
+          'and we create a client with a non-zero timeout': {
+            topic(FuzzyAIClient) {
+              try {
+                const client = new FuzzyAIClient({
+                  key: TOKEN,
+                  root: "http://localhost:2342",
+                  timeout: 5000
+                });
+                this.callback(null, client);
+              } catch (err) {
+                this.callback(err, null);
+              }
+              return undefined;
+            },
+            'it works'(err, client) {
+              assert.ifError(err);
+              return assert.isObject(client);
+            },
+            teardown(client) {
+              const { callback } = this;
+              client.stop(err => callback(null));
+              return undefined;
+            },
+            'and we get an agent': {
+              topic(client) {
+                client.getAgent(AGENTID, this.callback);
+                return undefined;
+              },
+              'it works'(err, agent) {
+                assert.ifError(err);
+                return assert.isObject(agent);
+              }
+            }
+          }
+        }
+      }
+    }}).export(module);
